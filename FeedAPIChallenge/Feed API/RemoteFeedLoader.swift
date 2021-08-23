6f8bfa7ebf2f51d291e9276dcf218d16a69184b7
Frank Carnevale <frank@fourward.org>
2021-08-23 08:59:35 -0500

    Complete `RemoteFeedLoader` `load` with failure invalidData error if not a 200 success response code

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -8,6 +8,8 @@ public final class RemoteFeedLoader: FeedLoader {
 	private let url: URL
 	private let client: HTTPClient
 
+	private static let OK_200 = 200
+
 	public enum Error: Swift.Error {
 		case connectivity
 		case invalidData
@@ -21,9 +23,12 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
+			case let .success((_, response)):
+				if response.statusCode != RemoteFeedLoader.OK_200 {
+					completion(.failure(Error.invalidData))
+				}
 			case .failure:
 				completion(.failure(Error.connectivity))
-			default: break
 			}
 		}
 	}
